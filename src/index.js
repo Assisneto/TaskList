7cbd4a5eeeb36310c36b07bb844016ea709d2cc9
Assisneto <netodamasceno12@gmail.com>
2019-05-24 11:42:17 -0300

    Task List

--- src/index.js
+++ src/index.js
@@ -1,41 +1,94 @@
-import "./config/ReactotroConfig";
-import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import React, { Component } from 'react';
+import {
+  Platform, StyleSheet, ScrollView, View, Text,
+} from 'react-native';
 
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
-});
+import Post from './components/Post';
 
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
-}
+const white = '#FFF';
+const primary = '#EE7777';
+const title = '#333';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
+    backgroundColor: primary,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
+
+  header: {
+    backgroundColor: white,
+    justifyContent: 'center',
+    alignItems: 'center',
+    // No iOS precisamos aplicar 20px de padding superior
+    // já que a Status Bar não joga o conteúdo do App
+    // pra baixo automaticamente
+    ...Platform.select({
+      ios: {
+        height: 76,
+        paddingTop: 20,
+      },
+      android: {
+        height: 56,
+        paddingTop: 0,
+      },
+    }),
   },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
-  }
+
+  headerTitle: {
+    color: title,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
 });
+
+export default class App extends Component {
+  state = {
+    posts: [
+      {
+        id: 1,
+        title: 'Aprendendo React Native',
+        author: 'Assisneto',
+        content:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      },
+      {
+        id: 2,
+        title: 'Aprendendo React Native',
+        author: 'Assisneto',
+        content:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      },
+      {
+        id: 3,
+        title: 'Aprendendo React Native',
+        author: 'Assisneto',
+        content:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      },
+      {
+        id: 4,
+        title: 'Aprendendo React Native',
+        author: 'Assisneto',
+        content:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      },
+    ],
+  };
+
+  render() {
+    const { posts } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.headerTitle}>Task List</Text>
+        </View>
+        <ScrollView>
+          {posts.map(post => (
+            <Post key={post.id} data={post} />
+          ))}
+        </ScrollView>
+      </View>
+    );
+  }
+}
